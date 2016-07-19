4cc05c60f81869e07a04b81f14edd73fe105b3b8
mcannamela <m.cannamela@gmail.com>
2016-07-19 19:01:12 -0400

    method to hide debug nodes

--- breath.gd
+++ breath.gd
@@ -7,4 +7,12 @@ func _ready():
 	
 func _process(delta):
 	update()
+
+func hide_debug_nodes():
+	_get_damage_cone().hide()
 	
+func show_debug_nodes():
+	_get_damage_cone().hide()
+	
+func _get_damage_cone():
+	return get_node("damage_cone")
