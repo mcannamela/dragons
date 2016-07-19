5d97aaef86b1bdbee898958f6016680461392857
mcannamela <m.cannamela@gmail.com>
2016-07-18 23:08:24 -0400

    make dragon use refactored direction stuff for breath

--- dragon.gd
+++ dragon.gd
@@ -112,24 +112,7 @@ func _get_breath_input_direction():
 	return input_direction
 
 func _get_quantized_angle(angle):
-	# angle as from atan2, between -pi and pi
-	var angle_inc = PI/4.0	
-	
-	
-	# eliminate edge case
-	if angle < -7*PI/8 or angle > 7*PI/8:
-		return 0
-		
-	# all other cones contained in interval
-	var angle_lower_bound = -7*PI/8
-	var	angle_upper_bound = angle_lower_bound + angle_inc
-	
-	for i in range(7):
-		if angle >= angle_lower_bound and angle < angle_upper_bound:
-			return i + 1
-		else:
-			angle_lower_bound += angle_inc
-			angle_upper_bound += angle_inc
+	return get_node("breath_directionalizer").compute_quantized_angle(angle)
 			
 func _get_sprite_key_frame(quantized_angle):
 	var inc_to_frame_map = {
@@ -149,6 +132,11 @@ func _get_sprite_key_frame(quantized_angle):
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
+	var bd = get_node("breath_directionalizer")
+	bd.up_command = 'breathe_up'
+	bd.down_command = 'breathe_down'
+	bd.left_command = 'breathe_left'
+	bd.right_command = 'breathe_right'
 
 func _set_sprite_frame(n):
 	_get_sprite().set_frame(n)
@@ -172,5 +160,4 @@ func _set_speed_label():
 func _set_quantized_direction_label(q):
 	var s = "%d" % [q]
 	get_node("quantized_direction_label").set_text(s)
-	
 	
